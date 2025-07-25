be4d226c6242c5fc2ecdf7e9f8e15270eb73d4ef
Andrew <andreyka.zaytsev.2017@mail.ru>
2025-07-25 14:51:37 +0300

    creds

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,6 +2,7 @@ import { SERVER_HOST } from '../utils/env-constants.ts';
 import axios from 'axios';
 
 const api = axios.create({
+  withCredentials: true,
   baseURL: SERVER_HOST,
   headers: {
     'Content-Type': 'application/json',
